77dfe38406ab99d3af07214e22e0c492a85b1cf7
cds <due179@gmail.com>
2025-01-17 22:01:16 +0100

    Add setup.ps1 for installing on windows

--- README.md
+++ README.md
@@ -8,11 +8,9 @@ To install and configure everything for Ubuntu or Arch, just run the `setup.sh` 
 `sudo chmod 700 ./setup.sh && ./setup.sh`
 
 ### Windows
-To setup starship prompt for PowerShell run this
-`New-Item -ItemType Directory -Force ~/.config;New-Item -ItemType SymbolicLink -Path "~/.config/starship.toml" -Target "~/dotfiles/starship/starship.toml";`
+Run `./setup.ps1` in Powershell as Administrator.
 
 ## TODO
-- Add wezterm
 - Add komorebi tiling window manager for windows
 - Add windows status bar
 
---
+++ setup.ps1
@@ -0,0 +1,32 @@
+# Ensure the script is run as administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "This script must be run as Administrator!"
+    pause
+    exit 1
+}
+
+# Install wezterm
+winget install wez.wezterm
+
+# Install starship prompt
+winget install Starship.Starship
+
+# Install Neovim
+winget install Neovim.Neovim
+
+# Ultity for Neovim
+winget install BurntSushi.ripgrep.MSVC
+winget install fzf
+winget install sharkdp.fd
+
+# Zig as a C compiler
+winget install zig.zig
+
+# Create Symlinks
+New-Item -ItemType Directory -Force ~/.config
+New-Item -ItemType SymbolicLink -Path "~/.config/starship.toml" -Target "~/dotfiles/starship/starship.toml"
+New-Item -ItemType SymbolicLink -Path "~/.wezterm.lua" -Target "~/dotfiles/wezterm/wezterm.lua"
+New-Item -ItemType SymbolicLink -Path "~/AppData/Local/nvim" -Target "~/dotfiles/nvim"
+
+# Get nvim submodule
+git submodule update --init --recursive
