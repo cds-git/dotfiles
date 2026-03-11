05df7a0e8108064133d7d5cd954781ca914246ac
cds <cds@systematic.com>
2026-03-11 08:29:08 +0100

    rewrite nvim-dap with VS Code F-key bindings and .NET debug integration

--- nvim/lua/plugins/lsp/nvim-dap.lua
+++ nvim/lua/plugins/lsp/nvim-dap.lua
@@ -4,39 +4,26 @@ return {
 	config = function()
 		local dap = require("dap")
 
-        vim.keymap.set("n", "<F5>", dap.continue, {})
-		vim.keymap.set("n", "<F10>", dap.step_over, {})
-		vim.keymap.set("n", "<F11>", dap.step_into, {})
-		vim.keymap.set("n", "<F12>", dap.step_out, {})
-		-- vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<F2>", require("dap.ui.widgets").hover, {})
+		-- VS Code style F-key bindings
+		vim.keymap.set("n", "<F5>", dap.continue, { desc = "[DAP] Continue / Start" })
+		vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "[DAP] Toggle Breakpoint" })
+		vim.keymap.set("n", "<F10>", dap.step_over, { desc = "[DAP] Step Over" })
+		vim.keymap.set("n", "<F11>", dap.step_into, { desc = "[DAP] Step Into" })
+		vim.keymap.set("n", "<S-F11>", dap.step_out, { desc = "[DAP] Step Out" })
+		vim.keymap.set("n", "<F2>", require("dap.ui.widgets").hover, { desc = "[DAP] Hover" })
+		vim.keymap.set("n", "<S-F5>", dap.terminate, { desc = "[DAP] Stop / Terminate" })
+		vim.keymap.set("n", "<C-S-F5>", dap.run_last, { desc = "[DAP] Restart (Run Last)" })
 
-		require("utility.dotnet").setup_debug()
+		local dotnet = require("utility.dotnet")
+		dotnet.setup_debug()
+		dotnet.setup_commands()
 
-		vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
-		vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
-		vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
-
-		vim.fn.sign_define(
-			"DapBreakpoint",
-			{ text = " ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-		)
-		vim.fn.sign_define(
-			"DapBreakpointCondition",
-			{ text = "⚑ ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-		)
-		vim.fn.sign_define(
-			"DapBreakpointRejected",
-			{ text = " ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-		)
-		vim.fn.sign_define(
-			"DapLogPoint",
-			{ text = " ", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
-		)
-		vim.fn.sign_define(
-			"DapStopped",
-			{ text = "󰁕 ", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
-		)
+		-- Sign icons
+		vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "DiagnosticSignError", numhl = "" })
+		vim.fn.sign_define("DapBreakpointCondition", { text = "⚑ ", texthl = "DiagnosticSignError", numhl = "" })
+		vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "DiagnosticSignWarn", numhl = "" })
+		vim.fn.sign_define("DapLogPoint", { text = " ", texthl = "DiagnosticSignInfo", numhl = "" })
+		vim.fn.sign_define("DapStopped", { text = "󰁕 ", texthl = "DiagnosticSignOk", numhl = "" })
 	end,
 	keys = {
 		{
@@ -47,88 +34,11 @@ return {
 			desc = "[DAP] Breakpoint Condition",
 		},
 		{
-			"<leader>db",
-			function()
-				require("dap").toggle_breakpoint()
-			end,
-			desc = "[DAP] Toggle Breakpoint",
-		},
-		{
 			"<leader>dC",
 			function()
 				require("dap").clear_breakpoints()
 			end,
-			{ desc = "[DAP] Clear all breakpoints" },
-		},
-		{
-			"<leader>dc",
-			function()
-				require("dap").continue()
-			end,
-			desc = "[DAP] Continue",
-		},
-		{
-			"<leader>dR",
-			function()
-				require("dap").run_to_cursor()
-			end,
-			desc = "[DAP] Run to Cursor",
-		},
-		{
-			"<leader>dg",
-			function()
-				require("dap").goto_()
-			end,
-			desc = "[DAP] Go to Line (No Execute)",
-		},
-		{
-			"<leader>di",
-			function()
-				require("dap").step_into()
-			end,
-			desc = "[DAP] Step Into",
-		},
-		{
-			"<leader>dj",
-			function()
-				require("dap").down()
-			end,
-			desc = "[DAP] Down",
-		},
-		{
-			"<leader>dk",
-			function()
-				require("dap").up()
-			end,
-			desc = "[DAP] Up",
-		},
-		{
-			"<leader>dl",
-			function()
-				require("dap").run_last()
-			end,
-			desc = "[DAP] Run Last",
-		},
-		{
-			"<leader>dO",
-			function()
-				require("dap").step_out()
-			end,
-			desc = "[DAP] Step Out",
-		},
-		{
-			"<leader>do",
-			function()
-				require("dap").step_over()
-			end,
-			desc = "[DAP] Step Over",
-		},
-		{
-			"<leader>dp",
-			function()
-				require("dap").pause()
-			end,
-			desc = "[DAP] Pause",
+			desc = "[DAP] Clear All Breakpoints",
 		},
 		{
 			"<leader>dr",
@@ -138,55 +48,27 @@ return {
 			desc = "[DAP] Toggle REPL",
 		},
 		{
-			"<leader>dS",
+			"<leader>da",
 			function()
-				require("dap").session()
+				require("utility.dotnet").attach_to_process()
 			end,
-			desc = "[DAP] Session",
+			desc = "[DAP] Attach to .NET Process",
 		},
 		{
-			"<leader>dt",
+			"<leader>dd",
 			function()
-				require("dap").terminate()
+				require("utility.dotnet").debug_project()
 			end,
-			desc = "[DAP] Terminate",
+			desc = "[DAP] Debug .NET Project (pick & launch)",
 		},
-		{
-			"<leader>dw",
-			function()
-				require("dap.ui.widgets").hover()
-			end,
-			desc = "[DAP] Widgets",
-		},
-		-- {
-		-- 	"<F5>",
-		-- 	function()
-		-- 		-- (Re-)reads launch.json if present
-		-- 		if vim.fn.filereadable(".vscode/launch.json") then
-		-- 			require("dap.ext.vscode").load_launchjs(nil, { coreclr = { "cs" } })
-		-- 		end
-		-- 		require("dap").continue()
-		-- 	end,
-		-- 	{ desc = "[DAP] Start debugging from launch.json" },
-		-- },
 	},
 	dependencies = {
 		{
 			"theHamsta/nvim-dap-virtual-text",
 			opts = {
-				enabled = true, -- enable this plugin (the default)
-				enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-				highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-				highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-				show_stop_reason = true, -- show stop reason when stopped for exceptions
-				commented = false, -- prefix virtual text with comment string
-
-				-- experimental features:
-				virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
-				all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-				virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
-				virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
-				-- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+				highlight_changed_variables = true,
+				show_stop_reason = true,
+				virt_text_pos = "eol",
 			},
 		},
 		{
@@ -231,5 +113,4 @@ return {
 			end,
 		},
 	},
-
 }
