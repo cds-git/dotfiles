69fa11754827749357c66da48135f334c4228f36
cds <cds@systematic.com>
2026-03-17 15:37:25 +0100

    add better kubernetes yaml lsp support

--- nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/lua/plugins/lsp/lsp-config.lua
@@ -11,6 +11,41 @@ return {
 			},
 			-- root_markers = { ".git" },
 		})
+
+		vim.lsp.config("yamlls", {
+			settings = {
+				yaml = {
+					schemas = {
+						kubernetes = "*.yaml",
+						["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+						["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+						["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+						["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+						["http://json.schemastore.org/compose"] = "docker-compose*.{yml,yaml}",
+						["http://json.schemastore.org/helmfile"] = "helmfile.{yml,yaml}",
+					},
+					schemaStore = {
+						enable = true,
+						url = "https://www.schemastore.org/api/json/catalog.json",
+					},
+					validate = true,
+					completion = true,
+					hover = true,
+				},
+			},
+		})
+
+		vim.lsp.config("helm_ls", {
+			settings = {
+				["helm-ls"] = {
+					yamlls = {
+						enabled = true,
+						path = "yaml-language-server",
+					},
+				},
+			},
+		})
+
 		vim.lsp.enable({
 			"lua_ls",
 			"marksman",
