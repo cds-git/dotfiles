d74903721c65074d1aa3aa4588e72e2b5af15b18
cds <due179@gmail.com>
2026-06-08 13:10:54 +0200

    go back to render-markdown with keybind to toggle

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,6 +1,16 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+	-- In-buffer markdown rendering (headings, code blocks, tables, checkboxes,
+	-- callouts, etc). Renders in normal mode and reveals raw text on the line
+	-- the cursor is on / in insert mode. No browser needed -- works in the
+	-- terminal, unlike the old browser-based markdown-preview.nvim.
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	ft = { "markdown" },
+	opts = {},
+	keys = {
+		{ "<leader>mp", "<cmd>RenderMarkdown toggle<CR>", desc = "Toggle markdown render" },
+	},
 }
